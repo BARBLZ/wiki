6a358d7375b528c601169ed80ac209b233e4e316
zzc <2512466133@qq.com>
2022-02-11 21:57:11 +0800

    2-9 增加自定义配置项的示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller  返回页面，前后端分离基本用不到
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+
     //常见的http请求方式有四种：GET,POST,PUT,DELETE
 //    @RequestMapping("/hello")  //写一个接口，表示支持以上四种访问方式
     @GetMapping("/hello")  //只支持GET请求
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
 
     @PostMapping("/hello/post")  //只支持GET请求
     public String helloPost(String name){
-        return "Hello World! Post,"+name;
+        return "Hello World! Post," + name;
     }
 }
