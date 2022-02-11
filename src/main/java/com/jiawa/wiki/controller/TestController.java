4c3db4785ac721011477e7b22ea40d1ba06a6d7b
zzc <2512466133@qq.com>
2022-02-11 21:35:37 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  //返回字符串
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     //常见的http请求方式有四种：GET,POST,PUT,DELETE
-    @RequestMapping("/hello")  //写一个接口，表示支持以上四种访问方式
+//    @RequestMapping("/hello")  //写一个接口，表示支持以上四种访问方式
+    @GetMapping("/hello")  //只支持GET请求
     public String hello(){
         return "Hello World!";
+    }
+
+
+    @PostMapping("/hello/post")  //只支持GET请求
+    public String helloPost(String name){
+        return "Hello World! Post,"+name;
     }
 }
