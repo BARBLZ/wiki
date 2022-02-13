a4ce4f84fc40858360ca084dab9c8ed658ee35f1
zzc <2512466133@qq.com>
2022-02-13 19:50:38 +0800

    3-4 mybatis使用示例：开发/test/lost接口，功能为查询test表所有数据

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController  //返回字符串
 //@Controller  返回页面，前后端分离基本用不到
@@ -12,6 +17,8 @@ public class TestController {
     @Value("${test.hello:TEST}")
     private String testHello;
 
+    @Resource
+    private TestService testService;
 
     //常见的http请求方式有四种：GET,POST,PUT,DELETE
 //    @RequestMapping("/hello")  //写一个接口，表示支持以上四种访问方式
@@ -24,5 +31,10 @@ public class TestController {
     @PostMapping("/hello/post")  //只支持GET请求
     public String helloPost(String name){
         return "Hello World! Post," + name;
+    }
+
+    @GetMapping("/test/list")  //只支持GET请求
+    public List<Test> list(){
+        return testService.list();
     }
 }
